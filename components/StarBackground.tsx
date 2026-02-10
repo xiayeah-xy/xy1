fba0f717e8f2a55420b49f0dc126d31411278497
xiayeah-xy <xiayeah@gmail.com>
2026-02-10 23:06:14 +0800

    Update StarBackground.tsx

--- components/StarBackground.tsx
+++ components/StarBackground.tsx
@@ -1,1 +1,37 @@
 
+import React, { useMemo } from 'react';
+
+const StarBackground: React.FC = () => {
+  const stars = useMemo(() => {
+    return Array.from({ length: 50 }).map((_, i) => ({
+      id: i,
+      left: `${Math.random() * 100}%`,
+      top: `${Math.random() * 100}%`,
+      size: `${Math.random() * 2 + 1}px`,
+      duration: `${Math.random() * 3 + 2}s`,
+      delay: `${Math.random() * 5}s`
+    }));
+  }, []);
+
+  return (
+    <div className="stars-container">
+      {stars.map(star => (
+        <div
+          key={star.id}
+          className="star"
+          style={{
+            left: star.left,
+            top: star.top,
+            width: star.size,
+            height: star.size,
+            // @ts-ignore
+            '--duration': star.duration,
+            animationDelay: star.delay
+          }}
+        />
+      ))}
+    </div>
+  );
+};
+
+export default StarBackground;
